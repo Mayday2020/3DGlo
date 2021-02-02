9cde3b2474bdcf5621ea1d8c67887c7420f8a1c6
Mayday2020 <60267058+Mayday2020@users.noreply.github.com>
2021-02-02 18:26:19 +0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,46 @@
+
+window.addEventListener('DOMContentLoaded', function(){
+    'use strict';
+    
+    // Timer
+    function countTimer(deadline){
+        let timerHours = document.querySelector('#timer-hours'),
+            timerMinutes = document.querySelector('#timer-minutes'),
+            timerSeconds = document.querySelector('#timer-seconds');
+
+        function getTimeRemaining () {
+            let dateStop = new Date(deadline).getTime(),
+            dateNow = new Date().getTime(),
+            timeRemaining = (dateStop - dateNow) / 1000,
+            seconds = Math.floor(timeRemaining % 60),
+            minutes = Math.floor((timeRemaining / 60) % 60),
+            hours = Math.floor((timeRemaining / 60 / 60) % 24);
+            return {timeRemaining, hours, minutes, seconds};
+        }
+        function updateClock() {
+            let timer = getTimeRemaining();
+            let idInterval = setInterval(updateClock, 1000);
+            if(timer.timeRemaining < 0) {
+                clearInterval(idInterval);
+            } else {
+                if (timer.hours < 10) {
+                    timerHours.textContent = '0' + timer.hours;
+                } else {
+                    timerHours.textContent = timer.hours;
+                }
+                if(timer.minutes < 10) {
+                    timerMinutes.textContent = '0' + timer.minutes;
+                } else {
+                    timerMinutes.textContent = timer.minutes;
+                }
+                if(timer.seconds < 10) {
+                    timerSeconds.textContent = '0' + timer.seconds;
+                } else {
+                    timerSeconds.textContent = timer.seconds;
+                }
+            }
+        }
+        updateClock();
+    }
+    countTimer('01 july 2021');
+});
