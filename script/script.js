d1a8af5d00ed08a604069f9f3ebce73ce99d64e9
Mayday2020 <60267058+Mayday2020@users.noreply.github.com>
2021-02-16 18:38:20 +0300

    lesson22calc

--- script/script.js
+++ script/script.js
@@ -208,9 +208,39 @@ window.addEventListener('DOMContentLoaded', function(){
         });
     };
     command();
-    //Validate
-    const validateCalc = ()=>{
-        
+    //Calculate
+    const calc = (price = 100)=>{
+        const calcBlock = document.querySelector('.calc-block'),
+            calcType = calcBlock.querySelector('.calc-type'),
+            calcSquare = calcBlock.querySelector('.calc-square'),
+            calcCount = calcBlock.querySelector('.calc-count'),
+            calcDay = calcBlock.querySelector('.calc-day'),
+            totalValue = calcBlock.querySelector('#total');
+        const countSum = ()=>{
+            let total = 0,
+                countValue = 1,
+                dayValue = 1;
+            const typeValue = calcType.options[calcType.selectedIndex].value,
+                squareValue = +calcSquare.value;
+            if (calcCount.value > 1) {
+                countValue += (calcCount.value - 1) / 10;
+            }
+            if (calcDay.value && calcDay.value < 5) {
+                dayValue *= 2;
+            } else if (calcDay.value && calcDay.value < 10) {
+                dayValue *= 1.5;
+            }
+            if (typeValue && squareValue) {
+                total = price * typeValue * squareValue * countValue * dayValue;
+            }
+            totalValue.textContent = Math.floor(total);
+        };
+        calcBlock.addEventListener('change', (event)=>{
+            const target = event.target;
+            if (target.matches('input') || target.matches('select')){
+                countSum();
+            }
+        });
     };
-    validateCalc();
+    calc(100);
 });
